b5eafac38a6087d5f5330a5a7245cfcb419e3658
Phats Lys <phatly2001@gmail.com>
2020-10-05 22:04:38 +0700

    about us form

---
+++ about.php
@@ -0,0 +1,203 @@
+	<?php 
+		 include_once("./template/header.php");
+	?>
+	<!-- breadcrumbs -->
+	<section class="inner-banner-main">
+		<div class="about-inner about editContent">
+			<div class="container">
+				<div class="main-titles-head ">
+					<h3 class="header-name editContent">
+						About Us
+					</h3>
+					<p class="tiltle-para editContent ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic
+						fuga sit illo modi aut aspernatur tempore laboriosam saepe dolores eveniet.</p>
+				</div>
+				<div class="breadcrumbs-sub">
+					<ul class="breadcrumbs-custom-path">
+						<li class="right-side propClone"><a href="index.php" class="editContent">Home <span
+									class="fa fa-angle-right" aria-hidden="true"></span></a>
+							<p>
+						</li>
+						<li class="active editContent">About</li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+
+		</div>
+	</section>
+	<!-- breadcrumbs //-->
+	<section class="content-with-photo-4" id="about">
+		<div class="content-with-photo4-block editContent">
+			<div class="container">
+				<div class="cwp4-two row">
+					<div class="cwp4-image col-xl-6">
+						<img src="assets/images/p8.jpeg" alt="Park" class="img-responsive about-me">
+					</div>
+					<div class="cwp4-text col-xl-6 ">
+						<div class="posivtion-grid">
+							<h6 class="sub-title">About Us</h6>
+							<h3 class="editContent">We Are Best For Your Entertainment </h3>
+							<p class="para editContent">Mollitia placeat modi explicabo voluptatum
+								adipisci Quisquam exercit tionem praesentium suscipit.unde Dicta in uaerat Lorem, ipsum
+								dolor sit amet consectetur adipisicing elit. Provident vel iusto illo laboriosam ut
+								neque. Vel unde sint error placeat. Alias modi molestias blanditiis aspernatur? Ex iusto
+								nam ipsum aliquam.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+	<section class="about ">
+		<div class="skills-bars editContent counter-width ">
+			<div class="container">
+				<div class="nature-bars">
+					<h3>Farm, Garden and Agriculture</h3>
+					<div class="link-list-menu">
+						<p>Consectetur adipiscing elit. Aliquam auctor Sed bibendum varius euismod. </p>
+					</div>
+					<a href="about.php" class="action-button btn mt-3">Read more</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="recent-work">
+		<div class="jst-two-col">
+			<div class="container">
+				<div class="row">
+
+					<div class="my-bio col-xl-6">
+						<h3>Our Experience</h3>
+						<p class="para mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.inventore deleniti
+							dolore sequi labore similique vitae quae est.</p>
+						<div class="details-details">
+							<div class="my-bar-grids">
+								<h4>Product Purity<span> 80% </span></h4>
+
+								<div class="progress">
+									<div class="progress-bar progress-bar-striped bg-color" role="progressbar"
+										style="width: 80% ;height:.3rem;" aria-valuenow="50" aria-valuemin="0"
+										aria-valuemax="100" div="">
+									</div>
+								</div>
+							</div>
+							<div class="my-bar-grids">
+								<h4>Farmers Competence<span> 60% </span></h4>
+
+								<div class="progress">
+									<div class="progress-bar progress-bar-striped bg-color" role="progressbar"
+										style="width: 60% ;height:.3rem;" aria-valuenow="50" aria-valuemin="0"
+										aria-valuemax="100" div="">
+									</div>
+								</div>
+							</div>
+							<div class="my-bar-grids">
+								<h4>Equipment<span> 70% </span></h4>
+
+								<div class="progress">
+									<div class="progress-bar progress-bar-striped bg-color" role="progressbar"
+										style="width: 70% ;height:.3rem;" aria-valuenow="50" aria-valuemin="0"
+										aria-valuemax="100" div="">
+									</div>
+								</div>
+							</div>
+							<div class="my-bar-grids">
+								<h4>Satisfied Customers<span> 60% </span></h4>
+
+								<div class="progress">
+									<div class="progress-bar progress-bar-striped bg-color" role="progressbar"
+										style="width: 60% ;height:.3rem;" aria-valuenow="50" aria-valuemin="0"
+										aria-valuemax="100" div="">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-6 ">
+						<img src="assets/images/b4.jpg" alt="product" class="img-responsive about-me">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="team-main-6" id="team">
+		<!-- /team-grids -->
+		<div class="team-content-page editContent text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-sm-6 team-team">
+						<div class="our-team">
+							<a href="#page">
+								<img src="assets/images/t1.jpg" alt="product" class="img-responsive ">
+								<div class="team-content">
+									<h3 class="name"><a href="#page">Kellie Smith</a></h3>
+									<span class="post"><a href="#page">Food Production</a< /span>
+										</a>
+										<div class="social-teams">
+											<a href="#" class="facebook"><span class="fa fa-facebook"></span></a>
+											<a href="#" class="twitter"><span class="fa fa-twitter"></span></a>
+											<a href="#" class="pinterest"><span class="fa fa-pinterest"></span></a>
+										</div>
+								</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 team-team">
+						<div class="our-team">
+							<a href="#page">
+								<img src="assets/images/t2.jpg" alt="product" class="img-responsive ">
+								<div class="team-content">
+									<h3 class="name"><a href="#page">Max Willson</a></h3>
+									<span class="post"><a href="#page">Stocker</a< /span>
+										</a>
+										<div class="social-teams">
+											<a href="#" class="facebook"><span class="fa fa-facebook"></span></a>
+											<a href="#" class="twitter"><span class="fa fa-twitter"></span></a>
+											<a href="#" class="pinterest"><span class="fa fa-pinterest"></span></a>
+										</div>
+								</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 team-team">
+						<div class="our-team">
+							<a href="#page">
+								<img src="assets/images/t3.jpg" alt="product" class="img-responsive ">
+								<div class="team-content">
+									<h3 class="name"><a href="#page">Jack Smith</a></h3>
+									<span class="post"><a href="#page">Warehouse</a< /span>
+										</a>
+										<div class="social-teams">
+											<a href="#" class="facebook"><span class="fa fa-facebook"></span></a>
+											<a href="#" class="twitter"><span class="fa fa-twitter"></span></a>
+											<a href="#" class="pinterest"><span class="fa fa-pinterest"></span></a>
+										</div>
+								</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 team-team">
+						<div class="our-team">
+							<a href="#page">
+								<img src="assets/images/t4.jpg" alt="product" class="img-responsive ">
+								<div class="team-content">
+									<h3 class="name"><a href="#page">Sam Deos</a></h3>
+									<span class="post"><a href="#page">Manager</a< /span>
+										</a>
+										<div class="social-teams">
+											<a href="#" class="facebook"><span class="fa fa-facebook"></span></a>
+											<a href="#" class="twitter"><span class="fa fa-twitter"></span></a>
+											<a href="#" class="pinterest"><span class="fa fa-pinterest"></span></a>
+										</div>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+		<!-- /team-grids -->
+	</section>
+	<?php 
+		 include_once("./template/footer.php");
+	?>
+	
