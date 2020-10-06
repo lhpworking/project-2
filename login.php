66da317582e8157f0209cedf283760ede74f74f9
Phats Lys <phatly2001@gmail.com>
2020-10-06 07:56:38 +0700

    log in form

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php 
+    include_once("../merciado/template/header.php");
+?>
+
+<!-- breadcrumbs -->
+<section class="inner-banner-main">
+    <div class="about-inner services editContent">
+        <div class="container">
+            <div class="main-titles-head ">
+                <h3 class="header-name editContent">
+                    Log In
+                </h3>
+                <p class="tiltle-para editContent editContent"> Welcome to the Merciado Amusement Park </p>
+            </div>
+            <div class="breadcrumbs-sub">
+                <ul class="breadcrumbs-custom-path">
+                    <li class="right-side propClone"><a href="index.html" class="editContent">Home <span
+                                class="fa fa-angle-right" aria-hidden="true"></span></a>
+                        <p>
+                    </li>
+                    <li class="active editContent">Log In</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    </div>
+</section>
+<!-- breadcrumbs //-->
+
+<!--Input information to sign up  -->
+<section class="contact-info-main" id="contact">
+    <div class="contact-sec	editContent">
+        <div class="container">
+            <div class="map-content-9">
+                <div class="twice">
+                    <input type="text" class="form-control" name="Subject" id="Subject" placeholder="Username"
+                        required="">
+                </div>
+                <div class="twice">
+                    <input type="password" class="form-control" name="Subject" id="Subject" placeholder="Password"
+                        required="">
+                </div>
+                <a href="#" class="forgotpass">Forgot your password ?</a>
+                <button type="submit" class="btn btn-contact" style="margin-left:100px">Log In</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php 
+    include_once("../merciado/template/footer.php");
+?>
